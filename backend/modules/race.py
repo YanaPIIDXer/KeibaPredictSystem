5830c3c14aa313922b5cbadefedc3f9536a64fee
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-10-08 08:11:19 +0900

    いろいろボトルネックになりそうな個所を改善

--- backend/modules/race.py
+++ backend/modules/race.py
@@ -13,19 +13,19 @@ class Race:
   # 学習・予測に必要なデータフレームを生成
   def build(self, horse_bank: HorseBank) -> pd.DataFrame:
     no = 1
-    df_list = []
+    data_list = []
     for h_name in self.__horses:
-      df = pd.DataFrame(index=[no])
-      data = horse_bank.get(h_name).build(self.__condition)
-      df['No'] = no
-      df['Name'] = h_name
-      df['Course'] = self.__course
-      df['Condition'] = self.__condition
-      df['ArrivalAvg'] = data['ArrivalAvg']
-      df['3FalongAvg'] = data['3FalongAvg']
+      data = {}
+      horse_data = horse_bank.get(h_name).build(self.__condition)
+      data['No'] = no
+      data['Name'] = h_name
+      data['Course'] = self.__course
+      data['Condition'] = self.__condition
+      data['ArrivalAvg'] = horse_data['ArrivalAvg']
+      data['3FalongAvg'] = horse_data['3FalongAvg']
       no += 1
-      df_list.append(df)
-    return pd.concat(df_list)
+      data_list.append(data)
+    return pd.DataFrame(data_list)
 
 # レース情報保管クラス
 class RaceBank:
