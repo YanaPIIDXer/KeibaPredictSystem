26672451ad9ea22c2bf78dc98aa27e615aa9c9a4
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-10-08 07:20:52 +0900

    事前処理したDataFrameを返すメソッド追加 とりあえず現状は過去３走分の平均着順のみ

--- backend/modules/horse.py
+++ backend/modules/horse.py
@@ -6,6 +6,15 @@ class Horse:
   def __init__(self, df: pd.DataFrame):
     self.__df = df.copy().sort_values('Date')
 
+  # 事前処理したDataFrameを返す
+  def preprocess(self) -> pd.DataFrame:
+    df = self.__df.copy()
+
+    # 過去３走の平均着順
+    df['ArrivalAvg'] = df['Arrival'].transform(lambda x: x.rolling(3, min_periods=1).mean().shift(1))
+    
+    return df
+
 # 馬情報保管クラス
 class HorseBank:
   # コンストラクタ
