5f83fee50dd650af1d847a7d6cc71b06031585cc
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-10-08 09:29:29 +0900

    近走の最終コーナーの位置取り平均を出すようにした

--- backend/modules/horse.py
+++ backend/modules/horse.py
@@ -5,9 +5,12 @@ class Horse:
   # コンストラクタ
   def __init__(self, df: pd.DataFrame):
     self.__df = df.copy().sort_values('Date')
-    # 過去３走の平均着順と上がり３ハロン
+    # 過去３走の平均着順
     self.__df['ArrivalAvg'] = self.__df['Arrival'].transform(lambda x: x.rolling(3, min_periods=1).mean().shift(1))
+    # 過去３走の上がり３ハロン平均
     self.__df['3FalongAvg'] = self.__df['3Falong'].transform(lambda x: x.rolling(3, min_periods=1).mean().shift(1))
+    # 過去３走の最終コーナーの位置取り平均
+    self.__df['LastCorner'] = self.__df['LastCorner'].transform(lambda x: x.rolling(3, min_periods=1).mean().shift(1))
 
   # 学習・予測に必要なデータを生成
   def build(self, condition: str) -> object:
