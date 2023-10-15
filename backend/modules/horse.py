3e306c113f8b23ab6c4567631bc1c7ba4a842703
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-10-16 06:55:41 +0900

    キャッシュを使って高速化

--- backend/modules/horse.py
+++ backend/modules/horse.py
@@ -11,10 +11,15 @@ class Horse:
     self.__df['3FalongAvg'] = self.__df['3Falong'].transform(lambda x: x.rolling(3, min_periods=1).mean().shift(1))
     # 過去３走の最終コーナーの位置取り平均
     self.__df['LastCornerAvg'] = self.__df['LastCorner'].transform(lambda x: x.rolling(3, min_periods=1).mean().shift(1))
+    self.__cond_dfs = dict()
 
   # 学習・予測に必要なデータを生成
   def build(self, condition: str) -> object:
-    cond_df = self.__df[self.__df['Condition'] == condition].copy()
+    if not condition in self.__cond_dfs:
+      cond_df = self.__df[self.__df['Condition'] == condition].copy()
+      self.__cond_dfs[condition] = cond_df
+    else:
+      cond_df = self.__cond_dfs[condition]
     # 引数に取った馬場状態と同じ馬場状態の過去３走分の上がり３ハロン
     cond_df['Condition3FalongAvg'] = cond_df['3Falong'].transform(lambda x: x.rolling(3, min_periods=1).mean().shift(1))
     
