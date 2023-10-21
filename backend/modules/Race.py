fa71cccb8e288a652f5072543d4d85f8143e0b2e
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-10-21 18:32:06 +0900

    色々と取得メソッド定義

--- backend/modules/Race.py
+++ backend/modules/Race.py
@@ -1,7 +1,28 @@
 import pandas as pd
+from typing import List
+import numpy as np
+
+# レースクラス
+class Race:
+  # コンストラクタ
+  def __init__(self, df):
+    self.__df = df
 
 # レース保管クラス
 class RaceBank:
   # コンストラクタ
   def __init__(self, pickle_path: str):
     self.__df = pd.read_pickle(pickle_path)
+
+  # 期間を指定して取得
+  def get_by_period(self, start: str, end: str) -> List[Race]:
+    df = self.__df[(self.__df['Date'] >= start) & (self.__df['Date'] <= end)]
+    races = []
+    df_list = np.array_split(df, len(df))
+    for d in df_list:
+      races.append(Race(d))
+    return races
+  
+  # 取得
+  def get(self, id: str) -> Race:
+    return Race(self.__df[self.__df['RaceID'] == id])
