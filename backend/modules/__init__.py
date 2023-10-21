929622f06b355dbe507e2f8f25d57c8bdb252f83
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-10-21 18:54:57 +0900

    レース情報取得メソッド

--- backend/modules/__init__.py
+++ backend/modules/__init__.py
@@ -6,3 +6,11 @@ class Master:
   def __init__(self, pickle_root_path: str):
     self.__race_df = pd.read_pickle(pickle_root_path + '/races.pickle')
     self.__result_df = pd.read_pickle(pickle_root_path + '/results.pickle')
+
+  # 期間を指定してレース情報を取得
+  def get_race_by_period(self, start: str = '1990-01-01', end: str = '2999-12-31') -> pd.DataFrame:
+    return self.__race_df[(self.__race_df['Date'] >= start) & (self.__race_df['Date'] <= end)].copy()
+
+  # IDを指定してレース情報を取得
+  def get_race_by_id(self, id: str) -> pd.DataFrame:
+    return self.__race_df[self.__race_df['RaceID'] == id].copy()
