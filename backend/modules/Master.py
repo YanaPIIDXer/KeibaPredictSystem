2af36e6886f0e9fcab06c79e6a33c3c51c70a70d
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-10-21 18:12:12 +0900

    コメント追記

--- backend/modules/Master.py
+++ backend/modules/Master.py
@@ -1,7 +1,9 @@
 from .Race import RaceBank
 from .Results import ResultBank
 
+# 複数のマスタを統合して扱うマスタクラス
 class MasterBank:
+  # コンストラクタ
   def __init__(self, race_pickle_path, result_pickle_path):
     self.__race_bank = RaceBank(race_pickle_path)
     self.__result_bank = ResultBank(result_pickle_path)
